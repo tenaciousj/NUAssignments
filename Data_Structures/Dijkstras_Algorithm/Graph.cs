2e2280ce82eebc14c0756fbb5beb9f8da6779a96
jeanettepranin <jeanettepranin@gmail.com>
2015-05-13 13:45:25 -0500

    adding dijkstra's algorithm

---
+++ Data_Structures/Dijkstras_Algorithm/Graph.cs
@@ -0,0 +1,305 @@
+﻿//Assignment 7 for EECS 214
+//Out: May 30rd, 2014
+//Due: June 6th, 2014
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace Assignment_7
+{
+    //public class Graph : IEnumerable<int>
+    public class Graph
+    {
+        //ENSURE THAT YOU HAVE THE ":IComparable<GraphNode>" below
+        public class GraphNode : IComparable<GraphNode>
+        {
+            //ALL THE STUFF FROM ASSIGNMENT 6 and ONE ADDITIONAL FIELD, cost 
+            //Of course, if you need other variables, feel free to add them
+            private int cost;
+            Point position;
+            String name;
+            int val;
+            List<int> weights = new List<int>();
+            List<GraphNode> neighbors = new List<GraphNode>();
+            public GraphNode predecessor;
+
+            public GraphNode() { }
+
+            public GraphNode(int v)
+            {
+                val = v;
+            }
+            public GraphNode(String n)
+            {
+                name = n;
+            }
+            public GraphNode(String n, Point pos)
+            {
+                name = n;
+                position = pos;
+            }
+            public Point Pos
+            {
+                get { return position; }
+                set { position = value; }
+            }
+            public String Name
+            {
+                get { return name; }
+                set { name = value; }
+            }
+            public int Val
+            {
+                get { return val; }
+                set { val = value; }
+            }
+            public List<int> Weights
+            {
+                get { return weights; }
+                set { weights = value; }
+            }
+            public List<GraphNode> Neighbors
+            {
+                get { return neighbors; }
+                set { neighbors = value; }
+            }
+            //Property to get and set cost
+            public int Cost
+            {
+                get { return cost; }
+                set { cost = value; }
+            }
+
+            //YOU AVE TO INCLUDE THIS COMPARETO FUNCTION BECAUSE 
+            //THE GRAPHNODE IMPLEMENTS THE ICOMPARABLE INTERFACE
+            //DONT WORRY ABOUT IT, ITS A C# WAY OF COMPARING OBJECTS
+            //TO RETURN IF AN OBJECT IS GREATER THAN OR LESS THAN ANOTHER OBJECT
+            public int CompareTo(GraphNode other)
+            {
+                if (this.cost < other.cost) return -1;
+                else if (this.cost > other.cost) return 1;
+                else return 0;
+            }
+        }
+
+        private List<GraphNode> nodes = new List<GraphNode>();
+
+        public Graph()
+        {
+            GraphNode sanfran = new GraphNode("San Francisco", new Point(25, 40)); //0
+            GraphNode denver = new GraphNode("Denver", new Point(200, 100));//1
+            GraphNode dallas = new GraphNode("Dallas", new Point(250, 250));//2
+            GraphNode chicago = new GraphNode("Chicago", new Point(300, 60));//3
+            GraphNode newyork = new GraphNode("New York", new Point(450, 100));//4
+            GraphNode losangeles = new GraphNode("Los Angeles", new Point(35, 200));//5
+            GraphNode sandiego = new GraphNode("San Diego", new Point(55, 275));//6
+            GraphNode miami = new GraphNode("Miami", new Point(400, 325));//7
+
+            AddNode(sanfran);
+            AddNode(denver);
+            AddNode(dallas);
+            AddNode(chicago);
+            AddNode(newyork);
+            AddNode(losangeles);
+            AddNode(sandiego);
+            AddNode(miami);
+
+            //all flights to San Francisco
+            AddDirectedEdge(denver, sanfran, 90);
+            AddDirectedEdge(chicago, sanfran, 60);
+
+            //all flights to Los Angeles
+            AddDirectedEdge(sanfran, losangeles, 45);
+            AddDirectedEdge(denver, losangeles, 100);
+            AddDirectedEdge(dallas, losangeles, 80);
+            AddDirectedEdge(sandiego, losangeles, 50);
+
+            //all flights to San Diego
+            AddDirectedEdge(dallas, sandiego, 80);
+
+            //all flights to Denver
+            AddDirectedEdge(chicago, denver, 25);
+            AddDirectedEdge(newyork, denver, 100);
+
+            //all flights to Dallas
+            AddDirectedEdge(newyork, dallas, 125);
+            AddDirectedEdge(miami, dallas, 50);
+
+            //all flights to Chicago
+            AddDirectedEdge(newyork, chicago, 80);
+
+            //all flights to Miami
+            AddDirectedEdge(newyork, miami, 90);
+
+        }
+
+        /// <summary>
+        /// Should add the node to the list of nodes
+        /// </summary>
+        public void AddNode(GraphNode node)
+        {
+            // adds a node to the graph
+            nodes.Add(node);
+        }
+
+        /// <summary>
+        /// Creates a new graphnode from an input value and adds it to the list of graph nodes
+        /// </summary>
+        public void AddNode(int value)
+        {
+            GraphNode node = new GraphNode(value);
+            AddNode(node);
+        }
+
+        /// <summary>
+        /// Adds a directed edge from one GraphNode to another. 
+        /// Also insert the corresponding weight 
+        /// </summary>
+        public void AddDirectedEdge(GraphNode from, GraphNode to, int weight)
+        {
+            from.Neighbors.Add(to);
+            from.Weights.Add(weight);
+
+            to.Neighbors.Add(from);
+            to.Weights.Add(weight);
+        }
+
+        /// <summary>
+        /// Adds an undirected edge from one GraphNode to another. 
+        /// Remember this means that both the from and two are neighbors of each other
+        /// Also insert the corresponding weight 
+        /// </summary>
+        public void AddUndirectedEdge(GraphNode from, GraphNode to, int cost)
+        {
+            from.Neighbors.Add(to);
+            from.Weights.Add(cost);
+        }
+
+        //Check if a value exists in the graph or not
+        public bool Contains(int value)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (nodes[i].Val == value)
+                    return true;
+            }
+            return false;
+        }
+
+        //Check if a name exists in the graph or not
+        public bool Contains(String name)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (nodes[i].Name == name)
+                    return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Should remove a node and all its edges and return true if successful
+        /// </summary>
+        public bool Remove(int value)
+        {
+            GraphNode remove = new GraphNode();
+            if (!Contains(value))
+                return false;
+            for (int i = 0; i < Count; i++)
+            {
+                if (nodes[i].Val == value)
+                {
+                    remove = nodes[i];
+                    break;
+                }
+            }
+            if (remove == null)
+                return false;
+            nodes.Remove(remove);
+            foreach (GraphNode g in nodes)
+            {
+                int i = g.Neighbors.IndexOf(remove);
+                if (i != -1)
+                {
+                    g.Neighbors.RemoveAt(i);
+                    g.Weights.RemoveAt(i);
+                }
+            }
+            return true;
+        }
+
+
+        /// <summary>
+        /// Is a property and returns the list of nodes, should be a one-liner
+        /// </summary>
+        public List<GraphNode> Nodes
+        {
+            //fill this up, otherwise it'll generate a property/indexer error
+            get { return nodes; }
+        }
+
+
+        /// <summary>
+        /// Is a property and returns the number of nodes in the graph, should be one-liner
+        /// </summary>
+        public int Count
+        {
+            //fill this up, otherwise it'll generate a property/indexer error 
+            get { return nodes.Count; }
+        }
+
+        //YOU SHOULD HAVE ALL THE STUFF FROM ASSIGNMENT 6 and ADD DIJKSTRA'S ALGORITHM to the GRAPH CLASS
+        /// Implementation of Djikstra's Algorithm. 
+
+        public List<GraphNode> DijkstraShortestPath(GraphNode start, GraphNode finish)
+        {
+            List<GraphNode> returnList = new List<GraphNode>(); //constains the list of nodes to go from start to finish
+            PriorityQueue<GraphNode> PQ = new PriorityQueue<GraphNode>();
+
+            //NOTICE HOW YOU HAVE ACCESS TO ALL THE FUNCTIONS YOU NEED, i.e.
+            //PQ.Insert(GraphNode insertNode)
+            //PQ.ExtractMin()
+            //PQ.DecreaseKey(GraphNode item, newCost)
+
+            foreach (GraphNode g in Nodes)
+            {
+                if (g.Name == start.Name)
+                    g.Cost = 0;
+                else
+                    g.Cost = Int32.MaxValue;
+                g.predecessor = null;
+                PQ.Insert(g);
+            }
+            while (PQ.Count() != 0)
+            {
+                GraphNode u = PQ.ExtractMin();
+                if (u.Name == finish.Name)
+                {
+                    GraphNode trace = u;
+                    while (trace != null)
+                    {
+                        returnList.Add(trace);
+                        trace = trace.predecessor;
+                    }
+                    break;
+                }
+                foreach (GraphNode v in u.Neighbors)
+                {
+                    int w = u.Weights[u.Neighbors.IndexOf(v)];
+                    int newCost = u.Cost + w;
+                    if (newCost < v.Cost)
+                    {
+                        v.Cost = newCost;
+                        PQ.DecreaseKey(PQ.Data.IndexOf(v));
+                        v.predecessor = u;
+                    }
+                }
+            }
+
+            return returnList;
+        }
+    }
+}
---
+++ Data_Structures/Dijkstras_Algorithm/MainWindow.xaml.cs
@@ -0,0 +1,274 @@
+﻿//Assignment 7 for EECS 214
+//Out: May 30rd, 2014
+//Due: June 6th, 2014
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Assignment_7
+{
+    /// <summary>
+    /// THIS SHOULD REMAIN VERY SIMILAR TO ASSIGNMENT 6, INSTEAD OF CALLING BFS for SHORTEST PATH, CALL DIJKSTRA's 
+    /// ALGORITHM 
+    /// </summary>
+    ///Don't worry about this being defined as a partial class, a partial class just allows you to split up code in 
+    ///different files while telling the compiler to splice them together during compilation
+    public partial class MainWindow : Window
+    {
+        List<Graph.GraphNode> nil = new List<Graph.GraphNode>();
+        Graph myGraph = new Graph(); //for ShortestPath
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //Registering callback for the route finding button
+            searchRouteBtn.Click += searchRouteBtn_Click;
+            drawStructure(nil);
+        }
+
+        void searchRouteBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //Get the to and from airports from the fromInput and toInput textboxes in MainWindow.xaml.cs
+            //Find the cheapest flight path and draw it using drawstructure
+            
+            //CALL DijkstraShortestPath(fromNode, toNode);
+            //Follow it up with a call to drawstructure to highlight the paths
+            MessageBoxResult message;
+
+            if (fromInput.Text != "" && toInput.Text != "")
+            {
+                Graph.GraphNode from = new Graph.GraphNode(fromInput.Text);
+                Graph.GraphNode to = new Graph.GraphNode(toInput.Text);
+                List<Graph.GraphNode> path = myGraph.DijkstraShortestPath(from, to);
+                drawStructure(path);
+            }
+            else
+                message = MessageBox.Show("Type Airport names first");
+ 
+        }
+
+
+        //ALL YOUR OLD DRAWSTRUCTURE CODE
+        private void drawStructure(List<Graph.GraphNode> path)
+        {
+            canvas.Children.Clear();
+
+            SolidColorBrush highlightBrush = new SolidColorBrush(Color.FromRgb(200, 0, 0));
+            SolidColorBrush highlightStrokeBrush = new SolidColorBrush(Color.FromRgb(200, 0, 0));
+
+            //Connector from Denver to SanFran
+            Line c1 = new Line();
+            c1.X1 = myGraph.Nodes[0].Pos.X + 30 / 2; //30 is the width of the circle/node drawn
+            c1.Y1 = myGraph.Nodes[0].Pos.Y + 30 / 2; //30 is the height of the circle/node drawn;
+            c1.X2 = myGraph.Nodes[1].Pos.X + 30 / 2;
+            c1.Y2 = myGraph.Nodes[1].Pos.Y + 30 / 2;
+            c1.StrokeThickness = 2;
+            if (path.Contains(myGraph.Nodes[0]) && path.Contains(myGraph.Nodes[1]))
+                c1.Stroke = highlightStrokeBrush;
+            else
+                c1.Stroke = new SolidColorBrush(Color.FromRgb(150, 150, 175));
+            canvas.Children.Add(c1);
+
+            //Connector from SanFran to Los Angeles
+            Line c2 = new Line();
+            c2.X1 = myGraph.Nodes[0].Pos.X + 30 / 2; //30 is the width of the circle/node drawn
+            c2.Y1 = myGraph.Nodes[0].Pos.Y + 30 / 2; //30 is the height of the circle/node drawn;
+            c2.X2 = myGraph.Nodes[5].Pos.X + 30 / 2;
+            c2.Y2 = myGraph.Nodes[5].Pos.Y + 30 / 2;
+            c2.StrokeThickness = 2;
+            if (path.Contains(myGraph.Nodes[0]) && path.Contains(myGraph.Nodes[5]))
+                c2.Stroke = highlightStrokeBrush;
+            else
+                c2.Stroke = new SolidColorBrush(Color.FromRgb(150, 150, 175));
+            canvas.Children.Add(c2);
+
+            //Connector from Los Angeles to San Diego
+            Line c3 = new Line();
+            c3.X1 = myGraph.Nodes[5].Pos.X + 30 / 2; //30 is the width of the circle/node drawn
+            c3.Y1 = myGraph.Nodes[5].Pos.Y + 30 / 2; //30 is the height of the circle/node drawn;
+            c3.X2 = myGraph.Nodes[6].Pos.X + 30 / 2;
+            c3.Y2 = myGraph.Nodes[6].Pos.Y + 30 / 2;
+            c3.StrokeThickness = 2;
+            if (path.Contains(myGraph.Nodes[5]) && path.Contains(myGraph.Nodes[6]))
+                c3.Stroke = highlightStrokeBrush;
+            else
+                c3.Stroke = new SolidColorBrush(Color.FromRgb(150, 150, 175));
+            canvas.Children.Add(c3);
+
+            //Connector from Denver to Los Angeles
+            Line c4 = new Line();
+            c4.X1 = myGraph.Nodes[1].Pos.X + 30 / 2; //30 is the width of the circle/node drawn
+            c4.Y1 = myGraph.Nodes[1].Pos.Y + 30 / 2; //30 is the height of the circle/node drawn;
+            c4.X2 = myGraph.Nodes[5].Pos.X + 30 / 2;
+            c4.Y2 = myGraph.Nodes[5].Pos.Y + 30 / 2;
+            c4.StrokeThickness = 2;
+            if (path.Contains(myGraph.Nodes[1]) && path.Contains(myGraph.Nodes[5]))
+                c4.Stroke = highlightStrokeBrush;
+            else
+                c4.Stroke = new SolidColorBrush(Color.FromRgb(150, 150, 175));
+            canvas.Children.Add(c4);
+
+            //Connector from Chicago to Denver
+            Line c5 = new Line();
+            c5.X1 = myGraph.Nodes[3].Pos.X + 30 / 2; //30 is the width of the circle/node drawn
+            c5.Y1 = myGraph.Nodes[3].Pos.Y + 30 / 2; //30 is the height of the circle/node drawn;
+            c5.X2 = myGraph.Nodes[1].Pos.X + 30 / 2;
+            c5.Y2 = myGraph.Nodes[1].Pos.Y + 30 / 2;
+            c5.StrokeThickness = 2;
+            if (path.Contains(myGraph.Nodes[3]) && path.Contains(myGraph.Nodes[1]))
+                c5.Stroke = highlightStrokeBrush;
+            else
+                c5.Stroke = new SolidColorBrush(Color.FromRgb(150, 150, 175));
+            canvas.Children.Add(c5);
+
+            //Connector from New York to Denver
+            Line c6 = new Line();
+            c6.X1 = myGraph.Nodes[4].Pos.X + 30 / 2; //30 is the width of the circle/node drawn
+            c6.Y1 = myGraph.Nodes[4].Pos.Y + 30 / 2; //30 is the height of the circle/node drawn;
+            c6.X2 = myGraph.Nodes[1].Pos.X + 30 / 2;
+            c6.Y2 = myGraph.Nodes[1].Pos.Y + 30 / 2;
+            c6.StrokeThickness = 2;
+            if (path.Contains(myGraph.Nodes[4]) && path.Contains(myGraph.Nodes[1]))
+                c6.Stroke = highlightStrokeBrush;
+            else
+                c6.Stroke = new SolidColorBrush(Color.FromRgb(150, 150, 175));
+            canvas.Children.Add(c6);
+
+            //Connector from New York to Chicago
+            Line c7 = new Line();
+            c7.X1 = myGraph.Nodes[4].Pos.X + 30 / 2; //30 is the width of the circle/node drawn
+            c7.Y1 = myGraph.Nodes[4].Pos.Y + 30 / 2; //30 is the height of the circle/node drawn;
+            c7.X2 = myGraph.Nodes[3].Pos.X + 30 / 2;
+            c7.Y2 = myGraph.Nodes[3].Pos.Y + 30 / 2;
+            c7.StrokeThickness = 2;
+            if (path.Contains(myGraph.Nodes[4]) && path.Contains(myGraph.Nodes[3]))
+                c7.Stroke = highlightStrokeBrush;
+            else
+                c7.Stroke = new SolidColorBrush(Color.FromRgb(150, 150, 175));
+            canvas.Children.Add(c7);
+
+            //Connector from New York to Miami
+            Line c8 = new Line();
+            c8.X1 = myGraph.Nodes[4].Pos.X + 30 / 2; //30 is the width of the circle/node drawn
+            c8.Y1 = myGraph.Nodes[4].Pos.Y + 30 / 2; //30 is the height of the circle/node drawn;
+            c8.X2 = myGraph.Nodes[7].Pos.X + 30 / 2;
+            c8.Y2 = myGraph.Nodes[7].Pos.Y + 30 / 2;
+            c8.StrokeThickness = 2;
+            if (path.Contains(myGraph.Nodes[4]) && path.Contains(myGraph.Nodes[7]))
+                c8.Stroke = highlightStrokeBrush;
+            else
+                c8.Stroke = new SolidColorBrush(Color.FromRgb(150, 150, 175));
+            canvas.Children.Add(c8);
+
+            //Connector from San Diego to Dallas
+            Line c9 = new Line();
+            c9.X1 = myGraph.Nodes[6].Pos.X + 30 / 2; //30 is the width of the circle/node drawn
+            c9.Y1 = myGraph.Nodes[6].Pos.Y + 30 / 2; //30 is the height of the circle/node drawn;
+            c9.X2 = myGraph.Nodes[2].Pos.X + 30 / 2;
+            c9.Y2 = myGraph.Nodes[2].Pos.Y + 30 / 2;
+            c9.StrokeThickness = 2;
+            if (path.Contains(myGraph.Nodes[6]) && path.Contains(myGraph.Nodes[2]))
+                c9.Stroke = highlightStrokeBrush;
+            else
+                c9.Stroke = new SolidColorBrush(Color.FromRgb(150, 150, 175));
+            canvas.Children.Add(c9);
+
+            //Connector from Dallas to Los Angeles
+            Line c10 = new Line();
+            c10.X1 = myGraph.Nodes[2].Pos.X + 30 / 2; //30 is the width of the circle/node drawn
+            c10.Y1 = myGraph.Nodes[2].Pos.Y + 30 / 2; //30 is the height of the circle/node drawn;
+            c10.X2 = myGraph.Nodes[5].Pos.X + 30 / 2;
+            c10.Y2 = myGraph.Nodes[5].Pos.Y + 30 / 2;
+            c10.StrokeThickness = 2;
+            if (path.Contains(myGraph.Nodes[2]) && path.Contains(myGraph.Nodes[5]))
+                c10.Stroke = highlightStrokeBrush;
+            else
+                c10.Stroke = new SolidColorBrush(Color.FromRgb(150, 150, 175));
+            canvas.Children.Add(c10);
+
+            //Connector from New York to Dallas
+            Line c11 = new Line();
+            c11.X1 = myGraph.Nodes[4].Pos.X + 30 / 2; //30 is the width of the circle/node drawn
+            c11.Y1 = myGraph.Nodes[4].Pos.Y + 30 / 2; //30 is the height of the circle/node drawn;
+            c11.X2 = myGraph.Nodes[2].Pos.X + 30 / 2;
+            c11.Y2 = myGraph.Nodes[2].Pos.Y + 30 / 2;
+            c11.StrokeThickness = 2;
+            if (path.Contains(myGraph.Nodes[4]) && path.Contains(myGraph.Nodes[2]))
+                c11.Stroke = highlightStrokeBrush;
+            else
+                c11.Stroke = new SolidColorBrush(Color.FromRgb(150, 150, 175));
+            canvas.Children.Add(c11);
+
+            //Connector from Dallas to Miami
+            Line c12 = new Line();
+            c12.X1 = myGraph.Nodes[2].Pos.X + 30 / 2; //30 is the width of the circle/node drawn
+            c12.Y1 = myGraph.Nodes[2].Pos.Y + 30 / 2; //30 is the height of the circle/node drawn;
+            c12.X2 = myGraph.Nodes[7].Pos.X + 30 / 2;
+            c12.Y2 = myGraph.Nodes[7].Pos.Y + 30 / 2;
+            c12.StrokeThickness = 2;
+            if (path.Contains(myGraph.Nodes[2]) && path.Contains(myGraph.Nodes[7]))
+                c12.Stroke = highlightStrokeBrush;
+            else
+                c12.Stroke = new SolidColorBrush(Color.FromRgb(150, 150, 175));
+            canvas.Children.Add(c12);
+
+            //Connector from Denver to SanFran
+            Line c13 = new Line();
+            c13.X1 = myGraph.Nodes[0].Pos.X + 30 / 2; //30 is the width of the circle/node drawn
+            c13.Y1 = myGraph.Nodes[0].Pos.Y + 30 / 2; //30 is the height of the circle/node drawn;
+            c13.X2 = myGraph.Nodes[3].Pos.X + 30 / 2;
+            c13.Y2 = myGraph.Nodes[3].Pos.Y + 30 / 2;
+            c13.StrokeThickness = 2;
+            if (path.Contains(myGraph.Nodes[0]) && path.Contains(myGraph.Nodes[3]))
+                c13.Stroke = highlightStrokeBrush;
+            else
+                c13.Stroke = new SolidColorBrush(Color.FromRgb(150, 150, 175));
+            canvas.Children.Add(c13);
+
+            //Feel free to use whatever colours you want
+            //I've iterated through the position and name lists here just to give an example
+            //you'll iterate through the graphnodes in your graph
+            int counter = 0;
+
+
+            foreach (Graph.GraphNode g in myGraph.Nodes)
+            {
+                Ellipse e = new Ellipse();
+                e.Height = 30;
+                e.Width = 30;
+                if (path.Contains(g))
+                    e.Fill = highlightBrush;
+                else
+                    e.Fill = new SolidColorBrush(Color.FromRgb(255, 190, 175));
+                e.StrokeThickness = 3;
+                e.Stroke = new SolidColorBrush(Color.FromRgb(175, 175, 175));
+                Canvas.SetLeft(e, g.Pos.X);
+                Canvas.SetTop(e, g.Pos.Y);
+                canvas.Children.Add(e);
+
+                Label l = new Label();
+                l.Content = myGraph.Nodes[counter].Name;
+                Canvas.SetLeft(l, g.Pos.X - 5);
+                Canvas.SetTop(l, g.Pos.Y - 20);
+
+                canvas.Children.Add(l);
+                counter++;
+            }
+
+        }
+
+    }
+}
